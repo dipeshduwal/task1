0d9a2a35747a86be766fea6d439651026b743723
dipeshduwal <electrifyingdipesh1@gmail.com>
2024-08-22 13:38:20 +0545

    Added array data into html table using innerhtml

--- javascript.js
+++ javascript.js
@@ -1,10 +1,33 @@
 
-const customerdata = [
-    {ID:1, name:'Adam', address:'US'},
-    {ID:2, name:'Bella', address:'UK'},
-    {ID:3, name:'Chris', address:'AUS'},
-    {ID:4, name:'Dan', address:'NEP'}
+const data = [
+    {ID:1, Name:'Adam', Address:'US'},
+    {ID:2, Name:'Bella', Address:'UK'},
+    {ID:3, Name:'Chris', Address:'AUS'},
+    {ID:4, Name:'Dan', Address:'NEP'},
+    {ID:5, Name:'Eva', Address:'NZ'}
 ];  
 
+function createTableWithInnerHTML() {
+    let tableHTML = '<table border="1"><tr>';
 
+    Object.keys(data[0]).forEach(key => {
+        tableHTML += `<th>${key}</th>`;
+    });
+
+    tableHTML += '</tr>';
+
+    data.forEach(item => {
+        tableHTML += '<tr>';
+        Object.values(item).forEach(value => {
+            tableHTML += `<td>${value}</td>`;
+        });
+        tableHTML += '</tr>';
+    });
+
+    tableHTML += '</table>';
+
+    document.body.innerHTML += tableHTML;
+}
+
+createTableWithInnerHTML();
 
