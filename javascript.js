717a39e415187280c813665ec5351aeae4cc8e87
dipeshduwal <electrifyingdipesh1@gmail.com>
2024-08-22 13:04:12 +0545

    Added Javascript array, accessing array into table

---
+++ javascript.js
@@ -0,0 +1,10 @@
+
+const customerdata = [
+    {ID:1, name:'Adam', address:'US'},
+    {ID:2, name:'Bella', address:'UK'},
+    {ID:3, name:'Chris', address:'AUS'},
+    {ID:4, name:'Dan', address:'NEP'}
+];  
+
+
+
