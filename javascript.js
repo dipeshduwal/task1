f9c9b1f40700960771c2ae5aa90f27017df348af
dipeshduwal <electrifyingdipesh1@gmail.com>
2024-08-22 17:44:44 +0545

    Comments and some changes

--- javascript.js
+++ javascript.js
@@ -1,27 +1,27 @@
-
 const data = [
     {ID:1, Name:'Adam', Address:'US'},
     {ID:2, Name:'Bella', Address:'UK'},
     {ID:3, Name:'Chris', Address:'AUS'},
     {ID:4, Name:'Dan', Address:'NEP'},
-    {ID:5, Name:'Eva', Address:'NZ'}
+    {ID:5, Name:'Eva', Address:'NZ'},
+    {ID:6, Name:'Fiona', Address:'US'}
 ];  
 
-function createTableWithInnerHTML() {
-    let tableHTML = '<table border="1"><tr>';
+function createTableWithInnerHTML() { //function to create table with innerhtml
+    let tableHTML = '<table border="1"><tr>'; //declaring css usable HTML table
 
-    Object.keys(data[0]).forEach(key => {
+    Object.keys(data[0]).forEach(key => { //declaring keys into tablehead from data
         tableHTML += `<th>${key}</th>`;
     });
 
-    tableHTML += '</tr>';
+    tableHTML += '</tr>'; //adding rows until the keys are available
 
-    data.forEach(item => {
+    data.forEach(item => {  
         tableHTML += '<tr>';
-        Object.values(item).forEach(value => {
+        Object.values(item).forEach(value => { //declaring values into tabledata for each item
             tableHTML += `<td>${value}</td>`;
         });
-        tableHTML += '</tr>';
+        tableHTML += '</tr>'; //adding rows until the values are available
     });
 
     tableHTML += '</table>';
@@ -30,4 +30,8 @@ function createTableWithInnerHTML() {
 }
 
 createTableWithInnerHTML();
+
+
+
+
 
